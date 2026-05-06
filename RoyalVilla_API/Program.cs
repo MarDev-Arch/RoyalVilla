3b094af5c100bae37f2d323e04fb5ab0c73868bf
Dell <Dell@DESKTOP-6SPESNN>
2026-05-06 15:52:17 +0100

    add true to health status

--- RoyalVilla_API/Program.cs
+++ RoyalVilla_API/Program.cs
@@ -97,7 +97,7 @@ builder.Services.AddAutoMapper(o =>
 builder.Services.AddScoped<IAuthService, AuthService>();
 //end
 var app = builder.Build();
-app.MapGet("/health", () => Results.Ok("Healthy"));
+app.MapGet("/health", () => Results.Ok(true));
 // add core start
 app.UseCors("AllowReactApp");
 //add cors end 
