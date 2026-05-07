79914f810c8940c02baeb88ffa582674104ea347
Dell <Dell@DESKTOP-6SPESNN>
2026-05-07 18:32:21 +0100

    loging request changed

--- RoyalVilla_API/Models/DTO/LoginRequestDTO.cs
+++ RoyalVilla_API/Models/DTO/LoginRequestDTO.cs
@@ -6,8 +6,8 @@ namespace RoyalVilla_API.Models.DTO
     {
         [Required]
         [EmailAddress]
-        public required string email { get; set; }
+        public required string Email { get; set; }
         [Required]
-        public required string password { get; set; }
+        public required string Password { get; set; }
     }
 }
